f2d8cfb47c990fc89986f2beb79685d96e249545
mkbolaes <mkbolaes@gmail.com>
2021-11-10 13:10:47 -0500

    Implemented initialization of Wallet class with quantities set to 0.

--- CashHandling.py
+++ CashHandling.py
@@ -1,16 +1,15 @@
+import numpy as np
+
+usd_denominations = np.array([('W', 20), ('T', 10), ('F', 5), ('O', 1), ('Q', .25), ('D', .10), ('N', .05), ('P', .01)],
+                             dtype=[('id','U1'), ('cash_value', 'f4')])
+
 class Wallet:
     # A class that represents a mutable store of cash
     # Will be used as the Vending Machine's cash register
     def __init__(self):
         # money should be an array that stores identifier, value, and inventory
-        money = {'W':20,
-                 'T':10,
-                 'F':5,
-                 'O':1,
-                 'Q':0.25,
-                 'D':0.10,
-                 'N':0.05,
-                 'P':0.01}
+        self.money = {d['id']:0 for d in denominations}
+
         pass
 
     def add_cash(self):
